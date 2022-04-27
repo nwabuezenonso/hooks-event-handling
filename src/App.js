c9810144bb5ed4511134e289abc0f28af14cfd75
stephen <nwabuezesc@gmail.com>
2022-04-27 19:02:55 +0100

    setting up hide and show functionality

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { useState } from 'react'
 function App() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [showpasswords, setshowpasswords] = useState(true)
+  const [showpassword, setshowpassword] = useState(true)
   //making use of hooks in value and onchange event
   return (
     <div className="container">
@@ -17,19 +17,19 @@ function App() {
         onChange={(e)=>  setUsername(e.target.value) }
       />
       <input 
-        type= "password"
+        type= {showpassword ? 'text' : 'password'}
         placeholder="Enter password"
-        value= { password }
+        value= {password}
         onChange={(e)=>  setPassword(e.target.value) }
       />
 
       {/* section for displaying the data */}
       <h2>{username}</h2>
       {/* if the showpass is true display the password else show the * as many times as the password number */}
-      <h2>{showpasswords ? password: '*'.repeat(password.length) }</h2>
+      <h2>{showpassword ? password: '*'.repeat(password.length) }</h2>
 
       {/* onclick handler tp set the password */}
-      <button onClick={(e)=> setshowpasswords(!showpasswords)}>Show/Hide password</button>
+      <button onClick={(e)=> setshowpassword(!showpassword)}>Show/Hide password</button>
     </div>
   );
 }
