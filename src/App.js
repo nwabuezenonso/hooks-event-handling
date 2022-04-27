5282df534c39578582098744c73b65a95f09efb4
stephen <nwabuezesc@gmail.com>
2022-04-27 18:52:01 +0100

    creating form with react hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import React from 'react'
+//use state hooks allow us to create state
+import React, { useState } from 'react'
 
 function App() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [showpasswords, setshowpasswords] = useState(true)
+  //making use of hooks in value and onchange event
   return (
-    <div className="App">
-      <div></div>
+    <div className="container">
+
+      {/* adding input to the application and setting an onchange event */}
+      <input 
+        type= "text"
+        placeholder="Enter username"
+        value= { username}
+        onChange={(e)=>  setUsername(e.target.value) }
+      />
+      <input 
+        type= "password"
+        placeholder="Enter password"
+        value= { password }
+        onChange={(e)=>  setPassword(e.target.value) }
+      />
+
+      {/* section for displaying the data */}
+      <h2>{username}</h2>
+      {/* if the showpass is true display the password else show the * as many times as the password number */}
+      <h2>{showpasswords ? password: '*'.repeat(password.length) }</h2>
+
+      {/* onclick handler tp set the password */}
+      <button onClick={(e)=> setshowpasswords(!showpasswords)}>Show/Hide password</button>
     </div>
   );
 }
